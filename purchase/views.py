99f101231eb24556a44d938f80cba4114c261d64
mkabir <ag.kabir@gmail.com>
2024-03-17 11:10:00 +0100

    created templates static folder and base.html,index.html with views and urls

--- purchase/views.py
+++ purchase/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login,logout
 
 # Create your views here.
+def home(request):
+    return render(request, template_name = 'index.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
