da1b1fb502ec62c9a401d73b50e9266a76871433
mkabir <ag.kabir@gmail.com>
2024-03-17 19:49:00 +0100

    added login functionality and added welcome text in home page

--- purchase/views.py
+++ purchase/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login,logout
+from django.contrib.auth import login,logout, authenticate
 
 # Create your views here.
 def home(request):
@@ -8,3 +8,18 @@ def home(request):
 def logout_user(request):
     logout(request)
     return redirect('home')
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            next = request.GET.get('next')
+            return redirect(next or 'home')
+        else:
+            return redirect('login')
+    else:
+        return render(request, template_name = 'login.html')
